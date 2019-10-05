55664f858bcaab0ece2baa4acdc3e5fa08c9af95
Hmasters4664 <hmasudi@gmail.com>
2019-10-05 15:51:10 +0200

    need to implement calender

--- wtd/WhatToDo/models.py
+++ wtd/WhatToDo/models.py
@@ -1,6 +1,7 @@
-
+from django.core.exceptions import ValidationError
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
 from .validators import validate_characters, check_negative_number, check_zero_number
 from django.utils import timezone
@@ -249,3 +250,43 @@ class Post(models.Model):
     dateCreated = models.DateTimeField(auto_now_add=True)
     likes = models.ManyToManyField('Profile', blank=True, related_name='user_likes')
     public = models.BooleanField(_('Is Public'), default=False)
+
+
+class Schedule(models.Model):
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='creator')
+    day = models.DateField(u'Day of the event', help_text=u'Day of the event')
+    start_time = models.TimeField(u'Starting time', help_text=u'Starting time')
+    end_time = models.TimeField(u'Final time', help_text=u'Final time')
+    notes = models.TextField(u'Textual Notes', help_text=u'Textual Notes', blank=True, null=True)
+
+    class Meta:
+        verbose_name = u'Scheduling'
+        verbose_name_plural = u'Scheduling'
+
+    def check_overlap(self, fixed_start, fixed_end, new_start, new_end):
+        overlap = False
+        if new_start == fixed_end or new_end == fixed_start:  # edge case
+            overlap = False
+        elif (new_start >= fixed_start and new_start <= fixed_end) or (
+                new_end >= fixed_start and new_end <= fixed_end):  # innner limits
+            overlap = True
+        elif new_start <= fixed_start and new_end >= fixed_end:  # outter limits
+            overlap = True
+
+        return overlap
+
+    def get_absolute_url(self):
+        url = reverse('admin:%s_%s_change' % (self._meta.app_label, self._meta.model_name), args=[self.id])
+        return u'<a href="%s">%s</a>' % (url, str(self.start_time))
+
+    def clean(self):
+        if self.end_time <= self.start_time:
+            raise ValidationError('Ending times must be after starting times')
+
+        events = Schedule.objects.filter(day=self.day)
+        if events.exists():
+            for event in events:
+                if self.check_overlap(event.start_time, event.end_time, self.start_time, self.end_time):
+                    raise ValidationError(
+                        'There is an overlap with another event: ' + str(event.day) + ', ' + str(
+                            event.start_time) + '-' + str(event.end_time))
